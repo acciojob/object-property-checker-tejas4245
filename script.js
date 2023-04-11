81bbc788e4abfa27ecaeb6a1569ac56e84291387
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-04-11 12:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 	for(let i in sampleObject){
-		if(i===key) return true
+		if(i===key)  
+			return true
 	}
 	return false;
 }
